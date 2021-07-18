54038b690254f1221b49ff66e0141f5dadc3012f
chrissii0503 <68234865+chrissii0503@users.noreply.github.com>
2021-07-18 07:25:51 -0800

    Add files via upload

---
+++ Form.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Form.aspx.cs" Inherits="WebApp.Form" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:FileUpload ID="FileUpload" runat="server" AllowMultiple="True" Height="26px" Width="254px" />
+            <br />
+            <br />
+            <asp:Button ID="btnLoad" runat="server" Height="24px" OnClick="btnLoad_Click" Text="Upload" Width="114px" />
+            <br />
+            <br />
+            <hr />  
+    <asp:GridView ID="GridView" runat="server">  
+    </asp:GridView>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Form.aspx.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.OleDb;
+using System.Configuration;
+using System.IO;
+using System.Data;
+using System.Windows.Forms;
+
+namespace WebApp
+{
+    public partial class Form : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnLoad_Click(object sender, EventArgs e)
+        {
+
+            string csvPath = Server.MapPath("~/csvfiles/") + Path.GetFileName(FileUpload.PostedFile.FileName);
+            FileUpload.SaveAs(csvPath);
+
+  
+            DataTable dt = new DataTable();
+            dt.Columns.AddRange(new DataColumn[6] {
+        new DataColumn("Student Number", typeof(string)),
+        new DataColumn("Full Name", typeof(string)),
+        new DataColumn("Surname", typeof(string)),
+        new DataColumn("Course Code",typeof(string)),
+        new DataColumn("Course Description", typeof(string)),
+        new DataColumn("Grade", typeof(string))});
+
+
+            string csvData = File.ReadAllText(csvPath);
+
+ 
+            foreach (string row in csvData.Split('\n'))
+            {
+                if (!string.IsNullOrEmpty(row))
+                {
+                    dt.Rows.Add();
+                    int i = 0;
+
+           
+                    foreach (string cell in row.Split(','))
+                    {
+                        dt.Rows[dt.Rows.Count - 1][i] = cell;
+                        i++;
+                    }
+                }
+            }
+
+            GridView.DataSource = dt;
+            GridView.DataBind();
+        }
+    }
+}
+      
